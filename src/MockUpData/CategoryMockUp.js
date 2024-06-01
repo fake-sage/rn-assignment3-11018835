74a924fd255f0b0ec6cb2ee41e6d2caf0b2f7841
Nii Ohene Omane-Addo <151413334+fake-sage@users.noreply.github.com>
2024-06-01 17:21:50 +0000

    Add files via upload

---
+++ src/MockUpData/CategoryMockUp.js
@@ -0,0 +1,58 @@
+const CategoryMockUp=[
+    {
+        id: 0,
+        title: "Exercise",
+        task: "12 tasks",
+        image: require('../../assets/woman.png')
+    },
+    {
+        id: 1,
+        title: "Study",
+        task: "12 tasks",
+        image: require('../../assets/man.png')
+    },
+    {
+        id: 2,
+        title: "Code",
+        task: "12 tasks",
+        image: require('../../assets/woman.png')
+    },
+    {
+        id: 3,
+        title: "School",
+        task: "12 tasks",
+        image: require('../../assets/man.png')
+    },
+    {
+        id: 4,
+        title: "Relax",
+        task: "12 tasks",
+        image: require('../../assets/man.png')
+    },
+    {
+        id: 5,
+        title: "plan",
+        task: "12 tasks",
+        image: require('../../assets/man.png')
+    },
+    {
+        id: 6,
+        title: "Manage projects",
+        task: "12 tasks",
+        image: require('../../assets/man.png')
+    },
+    {
+        id: 7,
+        title: "Test",
+        task: "12 tasks",
+        image: require('../../assets/man.png')
+    },
+    {
+        id: 8,
+        title: "Sleep",
+        task: "12 tasks",
+        image: require('../../assets/man.png')
+    },
+    
+]
+export default CategoryMockUp
---
+++ src/MockUpData/OngoingTaskMockUp.js
@@ -0,0 +1,67 @@
+const OngoingTaskMockUp =[
+    {
+        id : 0,
+        text : "Web Development"
+    },
+    {
+        id : 1,
+        text : "Mobile App Development"
+    },
+    {
+        id : 2,
+        text : "Desktop App Development"
+    },
+    {
+        id : 3,
+        text : "Documentation review"
+    },
+    {
+        id : 4,
+        text : "Code Testing"
+    },
+    {
+        id : 5,
+        text : "Code Debugging"
+    },
+    {
+        id : 6,
+        text : "Setting up the environment"
+    },
+    {
+        id : 7,
+        text : "User Interface Design"
+    },
+    {
+        id : 8,
+        text : "User Experience Design"
+    },
+    {
+        id : 9,
+        text : "User Onboarding"
+    },
+    {
+        id : 10,
+        text :  "Server Setup"
+    },
+    {
+        id : 11,
+        text : "Stress Testing"
+    },
+    {
+        id : 12,
+        text : "Feature Development"
+    },
+    {
+        id : 13,
+        text : "Feature Testing"
+    },
+    {
+        id : 14,
+        text : "Feature Debugging"
+    },
+    {
+        id : 15,
+        text : "Feature Deployment"
+    },
+]
+export default OngoingTaskMockUp
